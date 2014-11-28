e0267f2b7ad1757426f5f08dba5ef22c2c4771c7
Jacob Hanshaw <jacobhanshaw0@gmail.com>
2014-11-28 18:27:38 -0500

    Fixed networking problems *HOPEFULLY*

--- DaGame/Assets/Scripts/UpdateFromServer.cs
+++ DaGame/Assets/Scripts/UpdateFromServer.cs
@@ -3,53 +3,63 @@ using System.Collections;
 
 public class UpdateFromServer : MonoBehaviour
 {
-    private float lastSynchronizationTime = 0f;
-    private float syncDelay = 0f;
-    private float syncTime = 0f;
-    private Vector3 syncStartPosition = Vector3.zero;
-    private Vector3 syncEndPosition = Vector3.zero;
+		public bool usesCharacterController;
+		private CharacterController controller;
 
-    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
-    {
-        Vector3 syncPosition = Vector3.zero;
-        Vector3 syncVelocity = Vector3.zero;
-        if (stream.isWriting)
-        {
-            syncPosition = rigidbody.position;
-            stream.Serialize(ref syncPosition);
+		private float lastSynchronizationTime = 0f;
+		private float syncDelay = 0f;
+		private float syncTime = 0f;
+		private Vector3 syncStartPosition = Vector3.zero;
+		private Vector3 syncEndPosition = Vector3.zero;
 
-            syncPosition = rigidbody.velocity;
-            stream.Serialize(ref syncVelocity);
-        }
-        else
-        {
-            stream.Serialize(ref syncPosition);
-            stream.Serialize(ref syncVelocity);
+		void OnSerializeNetworkView (BitStream stream, NetworkMessageInfo info)
+		{
+				Vector3 syncPosition = Vector3.zero;
+				Vector3 syncVelocity = Vector3.zero;
+				if (stream.isWriting) {
+						syncPosition = gameObject.transform.position;
+						stream.Serialize (ref syncPosition);
 
-            syncTime = 0f;
-            syncDelay = Time.time - lastSynchronizationTime;
-            lastSynchronizationTime = Time.time;
 
-            syncEndPosition = syncPosition + syncVelocity * syncDelay;
-            syncStartPosition = rigidbody.position;
-        }
-    }
+						if (usesCharacterController)
+								syncVelocity = controller.velocity;
+						else
+								syncVelocity = rigidbody.velocity;
+						stream.Serialize (ref syncVelocity);
+				} else {
+						stream.Serialize (ref syncPosition);
+						stream.Serialize (ref syncVelocity);
 
-    void Awake()
-    {
-        lastSynchronizationTime = Time.time;
-    }
+						syncTime = 0f;
+						syncDelay = Time.time - lastSynchronizationTime;
+						lastSynchronizationTime = Time.time;
 
-    void Update()
-    {
-        if (!networkView.isMine)
-            SyncedMovement();
-    }
+						syncEndPosition = syncPosition + syncVelocity * syncDelay;
+						syncStartPosition = gameObject.transform.position;
+				}
+		}
 
-    private void SyncedMovement()
-    {
-        syncTime += Time.deltaTime;
-        gameObject.transform.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
-    }
+		void Awake ()
+		{
+				lastSynchronizationTime = Time.time;
+		}
+
+		void Start ()
+		{
+				if (usesCharacterController)
+						controller = gameObject.GetComponent<CharacterController> ();
+		}
+
+		void Update ()
+		{
+				if (!networkView.isMine)
+						SyncedMovement ();
+		}
+
+		private void SyncedMovement ()
+		{
+				syncTime += Time.deltaTime;
+				gameObject.transform.position = Vector3.Lerp (syncStartPosition, syncEndPosition, syncTime / syncDelay);
+		}
 
 }
