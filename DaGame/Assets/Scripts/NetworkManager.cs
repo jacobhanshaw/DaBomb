a398413571cc357c8becf733387002e1dd7bb230
Jacob Hanshaw <jacobhanshaw0@gmail.com>
2014-11-28 19:12:46 -0500

    Run in background

--- DaGame/Assets/Scripts/NetworkManager.cs
+++ DaGame/Assets/Scripts/NetworkManager.cs
@@ -3,78 +3,74 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour
 {
-    private const string typeName = "DaBomb";
-	private const string gameName = PersonalConstants.ROOM_NAME;
+		private const string typeName = "DaBomb";
+		private const string gameName = PersonalConstants.ROOM_NAME;
 
-    private bool isRefreshingHostList = false;
-    private HostData[] hostList;
+		private bool isRefreshingHostList = false;
+		private HostData[] hostList;
 
-    public GameObject playerPrefab;
+		public GameObject playerPrefab;
 
-    void OnGUI()
-    {
-        if (!Network.isClient && !Network.isServer)
-        {
-            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
-                StartServer();
+		void OnGUI ()
+		{
+				if (!Network.isClient && !Network.isServer) {
+						if (GUI.Button (new Rect (100, 100, 250, 100), "Start Server"))
+								StartServer ();
 
-            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
-                RefreshHostList();
+						if (GUI.Button (new Rect (100, 250, 250, 100), "Refresh Hosts"))
+								RefreshHostList ();
 
-            if (hostList != null)
-            {
-                for (int i = 0; i < hostList.Length; i++)
-                {
-                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
-                        JoinServer(hostList[i]);
-                }
-            }
-        }
-    }
+						if (hostList != null) {
+								for (int i = 0; i < hostList.Length; i++) {
+										if (GUI.Button (new Rect (400, 100 + (110 * i), 300, 100), hostList [i].gameName))
+												JoinServer (hostList [i]);
+								}
+						}
+				}
+		}
 
-    private void StartServer()
-    {
-        Network.InitializeServer(5, 25000, !Network.HavePublicAddress());
-        MasterServer.RegisterHost(typeName, gameName);
-    }
+		private void StartServer ()
+		{
+				Application.runInBackground = true;
+				Network.InitializeServer (5, 25000, !Network.HavePublicAddress ());
+				MasterServer.RegisterHost (typeName, gameName);
+		}
 
-    void OnServerInitialized()
-    {
-		Debug.Log("Server init");
-    }
+		void OnServerInitialized ()
+		{
+				Debug.Log ("Server init");
+		}
 
-    void Update()
-    {
-        if (isRefreshingHostList && MasterServer.PollHostList().Length > 0)
-        {
-            isRefreshingHostList = false;
-            hostList = MasterServer.PollHostList();
-        }
-    }
+		void Update ()
+		{
+				if (isRefreshingHostList && MasterServer.PollHostList ().Length > 0) {
+						isRefreshingHostList = false;
+						hostList = MasterServer.PollHostList ();
+				}
+		}
 
-    private void RefreshHostList()
-    {
-        if (!isRefreshingHostList)
-        {
-            isRefreshingHostList = true;
-            MasterServer.RequestHostList(typeName);
-        }
-    }
+		private void RefreshHostList ()
+		{
+				if (!isRefreshingHostList) {
+						isRefreshingHostList = true;
+						MasterServer.RequestHostList (typeName);
+				}
+		}
 
 
-    private void JoinServer(HostData hostData)
-    {
-        Network.Connect(hostData);
-    }
+		private void JoinServer (HostData hostData)
+		{
+				Network.Connect (hostData);
+		}
 
-    void OnConnectedToServer()
-    {
-		Debug.Log("Server connect");
-        SpawnPlayer();
-    }
+		void OnConnectedToServer ()
+		{
+				Debug.Log ("Server connect");
+				SpawnPlayer ();
+		}
 
-    private void SpawnPlayer()
-    {
-        Network.Instantiate(playerPrefab, Vector3.up * 5, Quaternion.identity, 0);
-    }
+		private void SpawnPlayer ()
+		{
+				Network.Instantiate (playerPrefab, Vector3.up * 5, Quaternion.identity, 0);
+		}
 }
